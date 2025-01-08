4813bd7e13eb8441da4ba99b7367f5399ac0ff53
Chen <chenzalman96@gmail.com>
2025-01-08 10:52:01 +0200

    Those are the main files for this Commit

---
+++ app/src/main/java/com/example/shoppingmanagement/Ui/Item.java
@@ -0,0 +1,8 @@
+package com.example.shoppingmanagement.Ui;
+
+public class Item {
+
+    private String name;
+    private int amount;
+
+}
---
+++ app/src/main/java/com/example/shoppingmanagement/Ui/ItemAdapter.java
@@ -0,0 +1,9 @@
+package com.example.shoppingmanagement.Ui;
+
+
+
+import androidx.recyclerview.widget.RecyclerView;
+
+public class ItemAdapter extends RecyclerView.Adapter<Item> {
+
+}
